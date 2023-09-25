f133f4f8e74f04b47448045ecc9bf8adc8a3f83a
Tobore1 <ogbodutobore@gmail.com>
2023-09-25 10:13:34 -0700

    t1c1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,30 +5,23 @@
  * list_len - returns the number of elements in a linked list
  * @g: pointer to the list_t list
  *
- * Return: number of elements in h
- */
-/*
- * step in used
- *size_t list_len(const list_t *g)
-{
- size_t n = 0;Initialize a counter variable.
- while (g)While there are nodes in the list.
- {
- n++;Increment the counter.
- g = g->next;Move to the next node.
- }
- return (n);Return the total count.
- *
+ * Return: number of elements in the list
+ * more explanation
+ * Initialize a counter variable.
+ * While there are nodes in the list
+ * Increment the counter
+ * Move to the next node
+ * Return the total coun
  */
 size_t list_len(const list_t *g)
 {
-size_t n = 0;
+    size_t n = 0;
 
-while (g)
-{
-n++;
-g = g->next;
-}
-return (n);
-}
+    while (g)
+    {
+        n++;
+        g = g->next;
+    }
 
+    return (n);
+}
